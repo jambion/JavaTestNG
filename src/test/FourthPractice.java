76f1315128a93d30feefa3be4fd0fb7f6865719d
JXA <Ambion_J@yahoo.com>
2018-01-22 11:10:39 -0800

    Added new sample tests

---
+++ src/test/FourthPractice.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class FourthPractice {
+	@Test
+	public void WebLoginHouseLoan() {
+		// Selenium
+		System.out.println("Web login house");
+	}
+	
+	@Test
+	public void MobileLoginCarLoan() {
+		// Appium
+		System.out.println("Mobile Login House Loan");
+
+	}
+	
+	@Test 
+	public void LoginAPICarLoan() {
+		// Rest API automation
+		System.out.println("Login API House Loan");
+
+	}
+}
---
+++ src/test/ThirdPractice.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class ThirdPractice {
+	@Test
+	public void WebLoginCarLoan() {
+		// Selenium
+		System.out.println("Web login car");
+	}
+	
+	@Test
+	public void MobileLoginCarLoan() {
+		// Appium
+		System.out.println("Mobile Login Car Loan");
+
+	}
+	
+	@Test 
+	public void LoginAPICarLoan() {
+		// Rest API automation
+		System.out.println("Login API Car Loan");
+
+	}
+
+}
