7661531795590105624ddbb8c64d5d3f672dc166
JXA <Ambion_J@yahoo.com>
2018-01-22 09:52:32 -0800

    first commit

---
+++ src/test/Practice.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+
+	@Test
+	public void Demo() {
+		System.out.println("Test");
+	}
+	
+	@Test
+	public void Demo2() {
+		System.out.println("Test 2");
+	}
+
+
+}
